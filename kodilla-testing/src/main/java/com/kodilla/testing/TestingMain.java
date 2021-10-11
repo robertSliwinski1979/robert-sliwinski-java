5ab012c0802857acd7557c8c1336b1a1a83369de
RobertSliwinski <23robert@wp.pl>
2021-10-11 23:15:05 +0100

    Pierwszy test jednostkowy

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,38 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")){
+            System.out.println("test OK");
+        }else{
+            System.out.println("Error!");
+        }
+        Calculator calc = new Calculator();
+
+        int c = 25;
+
+        if (c == calc.add(10,15)){
+            System.out.println("Addition is correct");
+        }else {
+            System.out.println("Addition failed!");
+        }
+
+        int d = 10;
+
+        if (d == calc.subtract(20,10)){
+            System.out.println("Subtraction was successful");
+        }else{
+            System.out.println("Subtraction is wrong!");
+        }
+
+
     }
 }
