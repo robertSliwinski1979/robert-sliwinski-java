5facebaad9c5f04b625e0f2bbf9544ace54373bf
RobertSliwinski <23robert@wp.pl>
2022-01-09 21:25:20 +0100

    7.5

--- kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayMainClass.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayMainClass.java
@@ -9,16 +9,11 @@ public class ArrayMainClass implements ArrayOperations {
     }
 
     public static double getAverage(int[] num) {
-        // IntStream.range(0, num.length).filter(num::toString).forEach(s->System.out.println(s));
-       // IntStream.range(0, num.length).map(s -> s).forEach(s -> System.out.println(s));
-        System.out.println("druk1");
-//        IntStream.range(0, num.length).forEach(s->System.out.println(s));
-      //???  IntStream.range(0, num.length).filter(s[i]->i+1)
-        //IntStream.range(0, N).forEach(this::doSomething)
-        System.out.println("druk2");
-        System.out.println(IntStream.range(0, num.length).average().getAsDouble());
 
-        // double result = IntStream.range(0, num.length).average().getAsDouble();
+        Arrays.stream(num).forEach(s->System.out.println(s));
+       // System.out.println(Arrays.stream(num).average().getAsDouble());
+
+
         double result = Arrays.stream(num).average().getAsDouble();
         return result;
     }
